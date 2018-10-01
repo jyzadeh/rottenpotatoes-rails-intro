222dd8d5f9e8c13e0529fd77f9860508e45599e1
Jake <jakespamandothers@gmail.com>
2018-09-30 22:01:18 -0700

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
 
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,28 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    if(!params.has_key?(:sort) && !params.has_key?(:ratings))
-      if(session.has_key?(:sort) || session.has_key?(:ratings))
-        redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
-      end
-    end
-    @sort = params.has_key?(:sort) ? (session[:sort] = params[:sort]) : session[:sort]
-    @all_ratings = Movie.all_ratings.keys
-    @ratings = params[:ratings]
-    if(@ratings != nil)
-      ratings = @ratings.keys
-      session[:ratings] = @ratings
-    else
-      if(!params.has_key?(:commit) && !params.has_key?(:sort))
-        ratings = Movie.all_ratings.keys
-        session[:ratings] = Movie.all_ratings
-      else
-        ratings = session[:ratings].keys
-      end
-    end
-    @movies = Movie.order(@sort).find_all_by_rating(ratings)
-    @mark  = ratings
-
+    @movies = Movie.all
   end
 
   def new
@@ -36,7 +19,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
+    @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -47,7 +30,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -58,20 +41,5 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
-  def similar_movie
-    id = params[:id]
-    @movie = Movie.find_by_id(id)
-    if (@movie.director.length == 0)
-      flash[:notice] = "'#{@movie.title}' has no director info."
-      redirect_to movies_path
-    end
-    @movies = Movie.find_all_by_director(@movie.director)
-  end
-  
-  def find_class(header)
-    params[:sort] == header ? 'hilite' : nil
-  end
-  helper_method :find_class
 
 end
